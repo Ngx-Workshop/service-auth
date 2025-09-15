5ec73ab930ff708bd1a4bfd80257d6e3a6f45227
Wesley DuSell <dusellfam@gmail.com>
2025-09-15 00:44:42 -0400

    Adding active-user

--- libs/ngx-auth-client/src/index.ts
+++ libs/ngx-auth-client/src/index.ts
@@ -1,3 +1,7 @@
+export * from './decorators/active-user.decorator';
+export * from './enums/request-keys.enum';
+export * from './enums/role.enum';
+export * from './interfaces/active-user-data.interface';
 export * from './ngx-auth-client.module';
 export * from './ngx-auth-client.service';
 export * from './ngx-remote-auth.guard';
