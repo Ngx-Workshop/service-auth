3e6065103e3117ee784633be99db3dadd47bc73e
Wesley DuSell <dusellfam@gmail.com>
2025-09-15 02:08:11 -0400

    Testing the metadata endpoint

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -55,7 +55,7 @@ export class AuthenticationService {
       );
     } catch (err) {
       this.logger.warn(
-        `Failed to upsert UserMetadata for ${userId}: ${String(err)} : ${this.metadataClient.defaults.baseURL}`,
+        `Failed to upsert UserMetadata for ${userId}: ${String(err)} : PROP ${this.metadataClient.defaults.baseURL} : VAR ${process.env.USER_METADATA_URL}`,
       );
       // Non-blocking on purpose
     }
