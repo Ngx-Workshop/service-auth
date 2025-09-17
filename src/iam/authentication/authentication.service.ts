fe0881a118110f0417df735b54f4467b167a5807
Wesley DuSell <dusellfam@gmail.com>
2025-09-16 22:50:52 -0400

    Better logs

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -56,7 +56,8 @@ export class AuthenticationService {
     } catch (err) {
       // Non-blocking on purpose
       this.logger.warn(
-        `[Non-blocking] Failed to upsert UserMetadata for ${userId}: ${String(err)}`,
+        `[Non-blocking] Failed to upsert UserMetadata for ${userId}`,
+        err,
       );
     }
   }
